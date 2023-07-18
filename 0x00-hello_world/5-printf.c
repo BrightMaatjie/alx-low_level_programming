0e669fe9afad22516b6be58d16a5541f48f289ef
Bright <maatjieb@gmail.com>
2023-07-18 07:41:17 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - A programm that prints that prints a line using the print f function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
